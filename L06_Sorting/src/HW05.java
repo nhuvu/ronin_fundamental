9c353a1323bd7aa45fd7151cff9c89ff7548e961
Nhu Vu <nhu.vu48@gmail.com>
2025-02-27 22:49:35 +0700

    exercise L06

--- L06_Sorting/src/HW05.java
+++ L06_Sorting/src/HW05.java
@@ -1,5 +1,65 @@
+import java.util.Scanner;
+
 public class HW05 {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Nhập số chiếc gậy: ");
+        int n = scanner.nextInt();
+        int[] sticks = new int[n];
+        int[] covers = new int[n];
+        for (int i = 0; i < sticks.length; i++) {
+            System.out.printf("Nhập độ dài chiếc gậy thứ %d: ", i + 1);
+            sticks[i] = scanner.nextInt();
+        }
+        for (int i = 0; i < covers.length; i++) {
+            System.out.printf("Nhập độ dài hộp phép thứ %d: ", i + 1);
+            covers[i] = scanner.nextInt();
+        }
+        scanner.close();
 
+        //sort 2 array
+        bubbleSort(sticks);
+        bubbleSort(covers);
+        //loop số hộp, tất cả hộp đều thỏa mãn thì true, ko thì false
+        boolean b = true;
+        for(int i = 0; i < covers.length; i++){
+            if(!coverCanHandleStick(sticks, covers, i+1)){
+                b = false;
+                break;
+            }
+        }
+        System.out.println(b ? "YES" : "NO");
+    }
+
+    //Để đảm bảo đủ hộp cho số gậy thì hộp có độ dài thứ i phải chứa được i gậy
+    // (vd: hộp thứ 1 phải >= độ dài 1 gậy, hộp thứ 2 phải >= độ dài 2 gậy)
+    public static boolean coverCanHandleStick(int[] sticks, int[] covers, int idxOfCover){
+        for(int i = 0; i < idxOfCover; i++){
+            if(covers[i] < sticks[i]){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void bubbleSort(int[] arr){
+        int end = arr.length - 1;
+        while (true){
+            for(int i = 0; i < end; i++){
+                if(arr[i] > arr[i+1]){
+                    swap(arr, i, i+1);
+                }
+            }
+            end--;
+            if(end == 0){
+                return;
+            }
+        }
+    }
+
+    public static void swap(int[] arr, int a, int b){
+        int temp = arr[a];
+        arr[a] = arr[b];
+        arr[b] = temp;
     }
 }
--- L07_Recursion_Search/src/Ex02.java
+++ L07_Recursion_Search/src/Ex02.java
@@ -1,9 +1,33 @@
+import java.util.Scanner;
+
 public class Ex02 {
     /**
      * Cho mảng A[] gồm n phần tử đã được sắp xếp. Hãy đưa ra 1 nếu X có mặt trong mảng A[],
      * ngược lại đưa ra -1.
      * */
     public static void main(String[] args) {
+        int[] array = new int[]{1,2,3,4,5,6,7,8};
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Nhập số X cần tìm: ");
+        int X = scanner.nextInt();
+        scanner.close();
 
+        System.out.println(binarySearch(array, X));
+    }
+
+    public static int binarySearch(int[]arr, int number){
+        int start = 0;
+        int end = arr.length - 1;
+        while(start <= end){
+            int mid = (start + end)/2;
+            if (number < arr[mid]) {
+                end = mid - 1;
+            }else if(number > arr[mid]){
+                start = mid + 1;
+            }else {
+                return 1;
+            }
+        }
+        return -1;
     }
 }
